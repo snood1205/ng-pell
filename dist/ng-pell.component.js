612d945c04c039af726aff74c27ea9b36554c9e9
Eli Sadoff <esadoff@ardiangroup.com>
2017-07-25 15:52:52 -0400

    NGC compiled to see if this works

--- dist/ng-pell.component.js
+++ dist/ng-pell.component.js
@@ -1,12 +1,3 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 import { Component, Input } from '@angular/core';
 import pell from 'pell';
 var NgPellComponent = (function () {
@@ -20,24 +11,19 @@ var NgPellComponent = (function () {
             root: 'pell'
         });
     };
-    __decorate([
-        Input(),
-        __metadata("design:type", Array)
-    ], NgPellComponent.prototype, "actions", void 0);
-    __decorate([
-        Input(),
-        __metadata("design:type", Array)
-    ], NgPellComponent.prototype, "classes", void 0);
-    __decorate([
-        Input(),
-        __metadata("design:type", Function)
-    ], NgPellComponent.prototype, "onChange", void 0);
-    NgPellComponent = __decorate([
-        Component({
-            selector: 'ng-pell-component',
-            template: "<div class=\"pell\" id=\"pell\"></div>"
-        })
-    ], NgPellComponent);
+    NgPellComponent.decorators = [
+        { type: Component, args: [{
+                    selector: 'ng-pell-component',
+                    template: "<div class=\"pell\" id=\"pell\"></div>"
+                },] },
+    ];
+    /** @nocollapse */
+    NgPellComponent.ctorParameters = function () { return []; };
+    NgPellComponent.propDecorators = {
+        'actions': [{ type: Input },],
+        'classes': [{ type: Input },],
+        'onChange': [{ type: Input },],
+    };
     return NgPellComponent;
 }());
 export { NgPellComponent };
